b79632340076df9542b8d2c06485c7f380ed623f
nesty <nesty.rln@gmail.com>
2023-11-28 11:53:38 -0600

    Card wrapper

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -57,6 +57,7 @@ export async function fetchLatestInvoices() {
 export async function fetchCardData() {
   noStore()
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
